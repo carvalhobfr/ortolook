105af1f17523200893e9dc278241a96aed05037d
Matheus Carvalho <carvalho.bfr@gmail.com>
2021-01-11 02:24:50 +0000

    tirar borda de botão important 2

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -4,4 +4,17 @@ export const Container = styled.div`
   testeDiv {
     margin-top: 40px;
   }
+
+  button {
+    border: none !important;
+    outline: 0 !important;
+    cursor: pointer !important;
+    overflow: visible !important;
+    color: inherit !important;
+    background: transparent !important;
+  }
+
+  button:focus {
+    outline: 0 !important;
+  }
 `
