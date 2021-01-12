a6db7cad12bd14cc93d73d8e6f78050fa5190e3a
Matheus Carvalho <carvalho.bfr@gmail.com>
2021-01-12 19:47:48 +0000

    teste apple

--- src/components/Estetica/HeaderEstetica/styles.js
+++ src/components/Estetica/HeaderEstetica/styles.js
@@ -65,7 +65,10 @@ export const Contentor = styled.div`
     flex-direction: column !important;
     max-height: 150px;
     margin-bottom: 16px;
-    margin-top: 160px;
+    img {
+      padding: 20px 0;
+      width: 50% !important;
+    }
   }
   .coluna3 {
     text-align: end;
