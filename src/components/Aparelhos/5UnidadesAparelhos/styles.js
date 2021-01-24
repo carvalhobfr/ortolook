cfc8b8ac44ddedb577f36ba6215704d7c08ceaaf
Matheus Carvalho <carvalho.bfr@gmail.com>
2021-01-24 23:01:53 +0000

    teste

--- src/components/Aparelhos/5UnidadesAparelhos/styles.js
+++ src/components/Aparelhos/5UnidadesAparelhos/styles.js
@@ -266,4 +266,9 @@ export const Contentor = styled.div`
       }
     }
   }
+  @media not all and (min-resolution: 0.001dpcm) {
+    @media (max-width: 1200px) {
+      min-height: 800px;
+    }
+  }
 `
