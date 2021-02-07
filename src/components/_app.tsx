e6f27c236a98afeb29b24b312448d069aa6f1ad1
Matheus Carvalho <matheuscarvalho@MacBook-Air-de-Matheus.local>
2021-02-07 17:37:55 +0000

    ajustes rdstation

--- src/components/_app.tsx
+++ src/components/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
@@ -6,6 +6,13 @@ import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src =
+      'https://d335luupugsy2.cloudfront.net/js/loader-scripts/45f2dd6c-bfa7-4b84-b302-e65c08c3049c-loader.js'
+    script.async = true
+    document.body.appendChild(script)
+  })
   return (
     <>
       <Component {...pageProps} />
