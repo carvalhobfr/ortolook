edc922c488b3bcf183140dccfb99f95aad06d60b
Matheus Carvalho <matheuscarvalho@MacBook-Air-de-Matheus.home>
2021-05-27 12:28:46 +0200

    ajustes navbar

--- src/components/Aparelhos/0NavBar/styles.js
+++ src/components/Aparelhos/0NavBar/styles.js
@@ -6,6 +6,7 @@ export const Contentor = styled.div`
   top: 0;
   position: sticky;
   z-index: 100;
+
   .navbar-collapse {
     /* display: flex; */
     /* justify-content: space-between; */
@@ -21,16 +22,16 @@ export const Contentor = styled.div`
     padding: 0;
     top: 0;
     text-align: center;
-    /*     border: 1px solid #802b67; */
-    text-align: end;
-    padding: 8px 32px;
   }
 
   img {
     max-height: 32px;
   }
   .nav-link,
-  .dropdown-item {
+  .dropdown-item,
+  .modalContato,
+  .nav-item,
+  .nav-item a {
     font-family: inherit;
     font-style: normal;
     font-weight: 300;
@@ -55,5 +56,71 @@ export const Contentor = styled.div`
     .navbar {
       padding: 0.5rem;
     }
+    .navbar-collapse {
+      background-image: url(/images/fundoNavAP.png);
+      background-position: right;
+      background-repeat: no-repeat;
+      background-size: contain;
+      padding: 64px 16px;
+      margin-top: -20px;
+      min-height: 327px;
+      display: flex;
+      /*       .nav-item a {
+        margin-right: 20%;
+        margin-left: 50%;
+      } */
+    }
+
+    .navbar-toggler {
+      z-index: 3 !important;
+      margin: 0%;
+    }
+    .dropdown-menu {
+      background-color: transparent !important;
+      width: 100%;
+      margin: auto;
+      filter: drop-shadow(2px 4px 6px black);
+    }
+    .nav-link,
+    .dropdown-item {
+      color: #fff !important;
+      text-align: center;
+    }
+    .dropdown-item {
+      max-width: 50%;
+      margin-left: auto;
+      margin-right: 20%;
+    }
+  }
+  .navClick span {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 1.0%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+  }
+  .dropdown-menu a a {
+    margin: 0px !important;
+    text-align: center;
+  }
+
+  @media (max-width: 600px) {
+    .nav-item a,
+    .nav-item {
+      color: #fff !important;
+      text-align: center;
+    }
+    li.nav-item {
+      margin-right: 20%;
+      margin-left: 40%;
+    }
+    .navSec {
+      margin: 0;
+      /* margin-right: -90px; */
+    }
+  }
+  a {
+    text-decoration: none;
+  }
+  button {
+    /*     margin-top: 28px; */
+    margin-bottom: 16px;
+    font-size: 22px;
   }
 `
